98219eee3bb38238321472ecba9741fc84f6b46e
alienworld1 <astronomyguy0030@gmail.com>
2024-02-25 00:53:27 +0530

    recursive fibonacci function

--- fibonacci.js
+++ fibonacci.js
@@ -1,4 +1,4 @@
-function fibonacci(n) {
+function fibs(n) {
     const result = [0, 1];
     while (result.length < n) {
         result.push(result[result.length - 1] + result[result.length - 2]);
@@ -6,4 +6,10 @@ function fibonacci(n) {
     return result;
 }
 
-console.log(fibonacci(8));
+function fibsRec(n) {
+    if (n === 0) return [0,];
+    if (n === 1) return [0, 1,];
+    return fibsRec(n - 1) + fibsRec(n - 2);
+}
+
+console.log(fibsRec(8));
